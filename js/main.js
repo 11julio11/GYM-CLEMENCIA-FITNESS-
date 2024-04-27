f2616a319638f505ef2dae9ff1c490bc781b8b4b
David11 <romerojesusdavid76@gmail.com>
2024-04-26 19:53:10 -0500

    implementacion de nuevas secciones 26/02/2024

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.querySelector(".navbar");
-
-  // Función para agregar o quitar la clase 'navbar-fixed' dependiendo del desplazamiento
-  function handleScroll() {
-    if (window.scrollY > navbar.offsetTop) { // Usamos offsetTop en lugar de offsetHeight
-      navbar.classList.add("navbar-fixed");
-    } else {
-      navbar.classList.remove("navbar-fixed");
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
     }
-  }
-
-  // Evento para manejar el desplazamiento de la página
-  window.addEventListener("scroll", handleScroll);
-
-  // Evento para agregar la clase 'navbar-fixed' al pasar el cursor sobre el menú
-  navbar.addEventListener("mouseenter", function () { // Cambiamos mouseover a mouseenter
-    navbar.classList.add("navbar-fixed");
-  });
-
-  navbar.addEventListener("mouseleave", function () { // Cambiamos mouseout a mouseleave
-    handleScroll(); // Verificar el desplazamiento al quitar el cursor
-  });
-});
+}
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
